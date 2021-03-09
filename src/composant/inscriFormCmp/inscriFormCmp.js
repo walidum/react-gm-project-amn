7ea3100f516e4cc8841daea0283da148b960dfa5
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-03-09 22:05:07 +0100

    first commit

--- src/composant/inscriFormCmp/inscriFormCmp.js
+++ src/composant/inscriFormCmp/inscriFormCmp.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './inscriFormCmp.css';
+import {Button, FormControl, Input} from "@material-ui/core";
 
 const InscriForm = () => {
 
